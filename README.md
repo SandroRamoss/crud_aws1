d8894f625761b088982c4e42819dd7766fa8b7ae
unknown <sandroramoss96@gmail.com>
2024-08-11 21:48:10 -0500

    cambios aws

---
+++ README.md
@@ -0,0 +1,87 @@
+# Lambda Prueba
+
+Este proyecto proporciona una API para realizar operaciones CRUD básicas sobre usuarios. La API permite recuperar, agregar, actualizar y eliminar usuarios en la base de datos. Los métodos disponibles son GET, POST, PUT y DELETE.
+
+## Método GET
+
+**URL:**  
+`https://yf75h69pmb.execute-api.us-east-1.amazonaws.com/dev`
+
+**Ejemplo de respuesta:**
+```json
+[
+    {
+        "id_producto": 17,
+        "description": "Chettos",
+        "stock": 10,
+        "estado": 0
+    },
+    {
+        "id_producto": 18,
+        "description": "Oreo",
+        "stock": 10,
+        "estado": 1
+    }
+]
+```
+
+# Method POST
+
+**Cuerpo de la solicitud:**
+
+```json
+{
+ "description": "prueba",
+ "stock": 3,
+ "estado": 1
+}
+```
+**Ejemplo de respuesta:**
+
+```json
+{
+    "message": "Se registró el producto."
+}
+```
+# Method PUT
+
+**Cuerpo de la solicitud:**
+
+```json
+{
+    "id" : 3,
+    "nombre" : "Miguel",
+    "email" : "miguel@gmail.com"
+
+}
+```
+
+**Ejemplo de respuesta:**
+
+```json
+{
+ "id_producto": 17,
+ "estado": 0
+}
+```
+**Ejemplo de respuesta:**
+
+```json
+{
+    "message": "Se actualizó el valor."
+}
+```
+
+# Method DELETE
+
+**Cuerpo de la solicitud:**
+
+`https://yf75h69pmb.execute-api.us-east-1.amazonaws.com/dev?id_producto=19`
+
+**Ejemplo de respuesta:**
+
+```json
+{
+    "message": "Se eliminó el valor."
+}
+```
---
+++ src/index.mjs
@@ -0,0 +1,92 @@
+import mysql from 'mysql';
+
+const con = mysql.createConnection({
+  host: 'gym-database.cxg68g4se5vs.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  port:"3306",
+  password: '9c2ne4BK.',
+  database: 'BD_CRUD',
+});
+
+export const handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const query = (sql, values) => new Promise((resolve, reject) => {
+    con.query(sql, values, (err, res) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(res);
+    });
+  });
+
+  try {
+    // Detectar el método HTTP
+    switch (event.httpMethod) {
+      case 'POST': {
+        const { description, stock, estado } = JSON.parse(event.body);
+        
+        if (!description || !stock || !estado) {
+          return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Todos los campos son obligatorios: descripción, stock y estado.' }),
+          };
+        }
+        
+        const sql = "INSERT INTO tb_producto (description,stock, estado) VALUES (?,?,?)";
+        await query(sql, [description, stock, estado]);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Se registró el producto.' }),
+        };
+      }
+
+      case 'GET': {
+        const sql = "SELECT * FROM tb_producto";
+        const results = await query(sql);
+        return {
+          statusCode: 200,
+          body: JSON.stringify(results),
+        };
+      }
+
+      case 'PUT': {
+        const { id_producto, estado } = JSON.parse(event.body);
+        const sql = "UPDATE tb_producto SET estado = ? WHERE id_producto = ?";
+        await query(sql, [estado, id_producto]);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Se actualizó el valor.' }),
+        };
+      }
+
+      case 'DELETE': {
+        const id_producto = event.queryStringParameters?.id_producto;
+        if (!id_producto) {
+          return {
+            statusCode: 400,
+            body: JSON.stringify({ message: "El ID del producto es requerido." }),
+          };
+        }
+        const sql = "DELETE FROM tb_producto WHERE id_producto = ?";
+        await query(sql, [id_producto]);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Se eliminó el valor.' }),
+        };
+      }
+
+      default: {
+        return {
+          statusCode: 405,
+          body: JSON.stringify({ message: 'Método no permitido.' }),
+        };
+      }
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error: ' + err.message }),
+    };
+  }
+};
